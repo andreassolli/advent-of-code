4cc9f374f6eb372bb0ffc3c1da828df41c3c4f20
Andreas <andreasssolli@gmail.com>
2024-12-11 22:28:17 +0100

    Working on day9 part2

--- 2024/Day9.java
+++ 2024/Day9.java
@@ -2,10 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-
-import static java.lang.Integer.parseInt;
-
 public class Day9 {
     public static void main(String[] args) {
         String line = "";
@@ -16,6 +14,11 @@ public class Day9 {
             e.printStackTrace();
         }
 
+        //System.out.println(shift(line, true));
+        System.out.println(shift(line, false));
+    }
+
+    private static long shift(String line, boolean all){
         List<Integer> file = new ArrayList<>();
         for(int i = 0; i < line.length(); i++) {
             int id = i % 2 == 0 ? i / 2 : -1;
@@ -25,28 +28,85 @@ public class Day9 {
             }
         }
 
-        shift(file);
-
-        System.out.println(checksum(file));
+        if(all){
+            return(checksum(shiftAll(file), true));
+        } else {
+            return(checksum(shiftIf(file), false));
+        }
     }
 
-    private static void shift(List<Integer> file){
-        for(int i = file.size() - 1; i >= 0; i--){
+    private static List<Integer> shiftIf(List<Integer> file){
+        List<Integer> newFile = new ArrayList<>(file);
+        int lookingFor = 9;
+        int needed = 0;
+        int gap = 0;
+        for(int i = newFile.size() - 1; i >= 0; i--){
+            if(lookingFor == 0) break;
+            int id = newFile.get(i);
+            if(id != lookingFor && id > 0) {
+                System.out.println(lookingFor);
+                System.out.println(id);
+                int open = 0;
+                for(int j = 0; j < i; j++){
+                    if(newFile.get(j) == -1){
+                        open++;
+                        System.out.println("Open++ for " + j + " Total: " + open);
+                        System.out.println("Needed: " + needed);
+                        if(open == needed){
+                            System.out.println("Needed: " + needed);
+                            System.out.println("Current: " + lookingFor);
+                            System.out.println("Open: " + open);
+                            System.out.println(i);
+                            System.out.println("NewFile(i): " + newFile.get(i));
+                            for(int k = needed; k > 0; k--){
+                                newFile.set(1 + j - k, lookingFor);
+                                System.out.println(newFile.get(i+k+gap));
+                                newFile.set(i + k + gap, -1);
+                                System.out.println(newFile.get(i+k+gap));
+                            }
+                            break;
+                        }
+                    } else {
+                        open = 0;
+                    }
+                }
+                gap = 0;
+                if(id < lookingFor) lookingFor--;
+                needed = 0;
+            } else {
+                if(id == -1) gap++;
+                needed++;
+            }
+        }
+        for(int num: newFile){
+            System.out.println(num);
+        }
+        return newFile;
+    }
+
+    private static List<Integer> shiftAll(List<Integer> file){
+        List<Integer> newFile = new ArrayList<>(file);
+        for(int i = newFile.size() - 1; i >= 0; i--){
             for(int j = 0; j < i; j++){
-                if(file.get(j) == -1){
-                    file.set(j, file.get(i));
-                    file.remove(i);
+                if(newFile.get(j) == -1 && newFile.get(i) != -1){
+                    newFile.set(j, newFile.get(i));
+                    newFile.remove(i);
                     break;
                 }
             }
         }
+        for(int num: newFile){
+            System.out.println(num);
+        }
+        return newFile;
     }
 
-    private static int checksum(List<Integer> file){
-        int sum = 0;
+    private static long checksum(List<Integer> file, boolean all){
+        long sum = 0;
         for(int i = 0; i < file.size(); i++){
-            System.out.println(file.get(i));
-            sum += file.get(i) * i;
+            int id = file.get(i);
+            if (id == -1 && all) break;
+            else if (id != -1) sum += (long) id * i;
         }
         return sum;
     }
