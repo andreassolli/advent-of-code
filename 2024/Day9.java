7ab1ff2729460cd6039fc5be40c6964e4f4b77a7
Andreas <andreasssolli@gmail.com>
2024-12-12 15:33:53 +0100

    Refactored and finished day 9

--- 2024/Day9.java
+++ 2024/Day9.java
@@ -2,111 +2,115 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
+
 public class Day9 {
     public static void main(String[] args) {
         String line = "";
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("2024/inputs/Day9.txt"));
+        try (BufferedReader reader = new BufferedReader(new FileReader("2024/inputs/Day9.txt"))) {
             line = reader.readLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        //System.out.println(shift(line, true));
-        System.out.println(shift(line, false));
+        System.out.println(performBlockByBlockCompaction(line));
+        System.out.println(performWholeFileCompaction(line));
     }
 
-    private static long shift(String line, boolean all){
-        List<Integer> file = new ArrayList<>();
-        for(int i = 0; i < line.length(); i++) {
-            int id = i % 2 == 0 ? i / 2 : -1;
-            int size = Character.digit(line.charAt(i), 10);
-            for(int j = 0; j < size; j++){
-                file.add(id);
+    private static long performBlockByBlockCompaction(String line) {
+        List<Integer> disk = parseDisk(line);
+        compactByBlocks(disk);
+        return checksum(disk);
+    }
+
+    private static long performWholeFileCompaction(String line) {
+        List<Integer> disk = parseDisk(line);
+
+        int maxFileId = 0;
+        for (int i = 0; i < line.length(); i += 2) {
+            maxFileId++;
+        }
+        maxFileId--;
+
+        compactByFiles(disk, maxFileId);
+        return checksum(disk);
+    }
+
+    private static List<Integer> parseDisk(String line) {
+        List<Integer> disk = new ArrayList<>();
+        int fileId = 0;
+        for (int i = 0; i < line.length(); i++) {
+            int length = Character.digit(line.charAt(i), 10);
+            if (i % 2 == 0) {
+                for (int j = 0; j < length; j++) disk.add(fileId);
+                fileId++;
+            } else {
+                for (int j = 0; j < length; j++) disk.add(-1);
             }
         }
-
-        if(all){
-            return(checksum(shiftAll(file), true));
-        } else {
-            return(checksum(shiftIf(file), false));
-        }
+        return disk;
     }
 
-    private static List<Integer> shiftIf(List<Integer> file){
-        List<Integer> newFile = new ArrayList<>(file);
-        int lookingFor = 9;
-        int needed = 0;
-        int gap = 0;
-        for(int i = newFile.size() - 1; i >= 0; i--){
-            if(lookingFor == 0) break;
-            int id = newFile.get(i);
-            if(id != lookingFor && id > 0) {
-                System.out.println(lookingFor);
-                System.out.println(id);
-                int open = 0;
-                for(int j = 0; j < i; j++){
-                    if(newFile.get(j) == -1){
-                        open++;
-                        System.out.println("Open++ for " + j + " Total: " + open);
-                        System.out.println("Needed: " + needed);
-                        if(open == needed){
-                            System.out.println("Needed: " + needed);
-                            System.out.println("Current: " + lookingFor);
-                            System.out.println("Open: " + open);
-                            System.out.println(i);
-                            System.out.println("NewFile(i): " + newFile.get(i));
-                            for(int k = needed; k > 0; k--){
-                                newFile.set(1 + j - k, lookingFor);
-                                System.out.println(newFile.get(i+k+gap));
-                                newFile.set(i + k + gap, -1);
-                                System.out.println(newFile.get(i+k+gap));
-                            }
-                            break;
-                        }
-                    } else {
-                        open = 0;
+    private static void compactByBlocks(List<Integer> disk) {
+        for (int i = disk.size() - 1; i >= 0; i--) {
+            if (disk.get(i) != -1) {
+                for (int j = 0; j < i; j++) {
+                    if (disk.get(j) == -1) {
+                        disk.set(j, disk.get(i));
+                        disk.set(i, -1);
+                        break;
                     }
                 }
-                gap = 0;
-                if(id < lookingFor) lookingFor--;
-                needed = 0;
-            } else {
-                if(id == -1) gap++;
-                needed++;
             }
         }
-        for(int num: newFile){
-            System.out.println(num);
-        }
-        return newFile;
     }
 
-    private static List<Integer> shiftAll(List<Integer> file){
-        List<Integer> newFile = new ArrayList<>(file);
-        for(int i = newFile.size() - 1; i >= 0; i--){
-            for(int j = 0; j < i; j++){
-                if(newFile.get(j) == -1 && newFile.get(i) != -1){
-                    newFile.set(j, newFile.get(i));
-                    newFile.remove(i);
+    private static void compactByFiles(List<Integer> disk, int maxFileId) {
+        for (int fileId = maxFileId; fileId >= 0; fileId--) moveFileIfPossible(disk, fileId);
+    }
+
+    private static void moveFileIfPossible(List<Integer> disk, int fileId) {
+        int start = -1, end = -1;
+        for (int i = 0; i < disk.size(); i++) {
+            if (disk.get(i) == fileId) {
+                if (start == -1) start = i;
+                end = i;
+            }
+        }
+
+        if (start == -1) return;
+
+        int fileLength = end - start + 1;
+
+        int freeSegmentStart = -1;
+        int freeCount = 0;
+        int bestStart = -1;
+
+        for (int i = 0; i < start; i++) {
+            if (disk.get(i) == -1) {
+                if (freeSegmentStart == -1) freeSegmentStart = i;
+                freeCount++;
+                if (freeCount == fileLength) {
+                    bestStart = freeSegmentStart;
                     break;
                 }
+            } else {
+                freeSegmentStart = -1;
+                freeCount = 0;
             }
         }
-        for(int num: newFile){
-            System.out.println(num);
-        }
-        return newFile;
+
+        if (bestStart == -1) return;
+
+        for (int i = 0; i < fileLength; i++) disk.set(bestStart + i, fileId);
+        for (int i = start; i <= end; i++) disk.set(i, -1);
     }
 
-    private static long checksum(List<Integer> file, boolean all){
+    private static long checksum(List<Integer> disk) {
         long sum = 0;
-        for(int i = 0; i < file.size(); i++){
-            int id = file.get(i);
-            if (id == -1 && all) break;
-            else if (id != -1) sum += (long) id * i;
+        for (int i = 0; i < disk.size(); i++) {
+            int id = disk.get(i);
+            if (id != -1) sum += (long) id * i;
         }
         return sum;
     }
